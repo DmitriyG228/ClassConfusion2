0df9a2826b006fadae1fae8c7ff9b33d78121d6a
Zachary Mueller <muellerzr@gmail.com>
2020-03-16 19:55:24 -0500

    undid accident

--- ClassConfusion2/data.py
+++ ClassConfusion2/data.py
@@ -51,7 +51,7 @@ def get_names(x:TabDataLoader, idxs, mc=None, varlist=None, li=None):
   cols = math.ceil(math.sqrt(boxes))
   row = math.ceil(boxes/cols)
   cats = x.cat_names.filter(lambda x: '_na' not in x)
-  tbnames = cats + x.cont_names if varlist is not None else varlist
+  tbnames = cats + x.cont_names if varlist is None else varlist
   tbnames = list(tbnames) #Colab doesn't like `L`'s
   return [tbnames, boxes, cols, rows, _]
 
