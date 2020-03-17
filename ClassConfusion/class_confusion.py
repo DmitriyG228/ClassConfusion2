046ac1baf8443b4559fc1f5ec052036bd93fe461
Zachary Mueller <muellerzr@gmail.com>
2020-03-16 21:05:10 -0500

    done!

--- ClassConfusion/class_confusion.py
+++ ClassConfusion/class_confusion.py
@@ -29,7 +29,7 @@ def _get_names(x:TabDataLoader, idxs, mc=None, varlist=None, li=None):
 def _get_names(x:TfmdDL, idxs, mc=None, varlist=None, li=None):
   ranges = []
   tbnames = []
-  boxes = int(input('Please enter a value for `k`, or the top # images you will see: '))
+  boxes = int(input('Please enter a value for `k`, or the top # images you will see: \n'))
   for x in iter(mc):
     for y in range(len(li)):
       if x[0:2] == li[y]:
@@ -60,21 +60,21 @@ def get_losses(x:TabDataLoader, tl_idx, preds, combs):
 def get_losses(x:TfmdDL, tl_idx, preds, combs):
   "Get losses and original `x` from `DataLoaders`"
   groupings = []
-  preds =preds.argmax(dim=1)
+  preds = preds.argmax(dim=1)
   dset = x.dataset
   dec = [x.vocab[i] for i in preds]
   for c in combs:
     idxs = []
     for i, idx in enumerate(tl_idx):
       if dec[idx] == c[0] and dset.vocab[dset[int(i)][1]] == c[1]:
-        idxs.append(int(i))
-    groupings.append(dset[idxs])
+        idxs.append(int(idx))
+    groupings.append(idxs)
   return groupings
 
 # Cell
 @typedispatch
-def _plot(x:TfmdDL, interp, combs, combs_l, tab, i=None, boxes=None, cols=None, rows=None, ranges=None, figsize=(12,12), cut_off=100):
-  "Plot tabular graphs"
+def _plot(x:TfmdDL, interp, idxs, combs, tab, i=None, boxes=None, cols=None, rows=None, ranges=None, figsize=(12,12), cut_off=100):
+  "Plot top pictures per classes chosen"
   y = 0
   if ranges[i] < boxes:
     cols = math.ceil(math.sqrt(ranges[i]))
@@ -85,18 +85,16 @@ def _plot(x:TfmdDL, interp, combs, combs_l, tab, i=None, boxes=None, cols=None, 
     cols = math.ceil(math.sqrt(boxes))
     rows = math.ceil(boxes/cols)
   fig, ax = plt.subplots(rows, cols, figsize=figsize)
-
   [axi.set_axis_off() for axi in ax.ravel()]
-  for j, idx in enumerate(combs_l):
+  for j, idx in enumerate(idxs[i]):
     if boxes < y+1 or y > ranges[i]:
       break
     row = (int)(y/cols)
     col = y % cols
-    print(idx)
     img, lbl = x.dataset[idx]
     fn = x.items[idx]
-    fn = re.search('([^/*]+)_\d+.*$', str(fn)).group(0)
-    img.show(ctx=ax[row,col])
+    fn = re.search('([^\/]\d+.*$)', str(fn)).group(0)
+    img.show(ctx=ax[row,col], title=fn)
     y+=1
   plt.show(fig)
   plt.tight_layout()
